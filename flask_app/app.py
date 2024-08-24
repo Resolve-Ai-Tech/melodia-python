bdf4fc27bf3bec6d50337928ae8b68bddf0299ea
DevMaycon <dev.maycon08@gmail.com>
2024-08-24 12:34:09 -0300

    #112977 feat: Adicionando Novas Rotas

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,33 +1,40 @@
 from flask import Flask, request, jsonify
+from src.utils import parametrers_validation
 import psycopg2
-from psycopg2 import sql
 
 app = Flask(__name__)
-
-# Configurações do banco de dados
-DATABASE_CONFIG = {
-    'dbname': 'melodia',
-    'user': 'melodia',
-    'password': 'melodia',
-    'host': 'localhost',
-    'port': '5444'
-}
-
-# Obter conexão com o banco de dados
-def get_db_connection():
-    conn = psycopg2.connect(**DATABASE_CONFIG)
-    return conn
 
 # Rota de inicio
 @app.route('/', methods=['GET'])
 def home_index():
     return jsonify({'status': 'API Funcionando!'})
 
-# Rota de playlists geradas em preferencias
-# Rota de playlists geradas por região
-# Rota de playlists geradas com base na atividade
-# Rota de tendencias musicais
-# Rota de musicas baseadas em emoções
+
+@app.route('/playlists', methods=['POST'])
+def playlists():
+    # Verifica todos os parametros necessarios
+    required_parametrers = {"playlist_name": None}
+    validation = parametrers_validation(required_parametrers)
+    if validation:
+        return validation
+
+    # Lógica de playlists geradas com base na atividade
+    # Lógica de playlists geradas em preferencias
+    # Lógica de playlists geradas por região
+    return jsonify({'status': f'Playlist {required_parametrers["playlist_name"]} foi criada com sucesso!'})
+
+
+@app.route('/trends', methods=['GET'])
+def musicas_trends():
+    # Lógica de tendencias musicais
+    pass
+
+
+@app.route('/musicas', methods=['GET'])
+def musicas_por_pessoa()
+    # Lógica de musicas baseadas em emoções
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
