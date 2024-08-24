e56c6f140505b8ef9672a68ce99b55863305fc7b
DevMaycon <dev.maycon08@gmail.com>
2024-08-24 13:52:25 -0300

    #112977 fix: Removendo scripts desnecessarios.

--- docker/connect_docker_postgresql.sh
+++
@@ -1,2 +0,0 @@
-sudo docker start melodia_database
-sudo docker exec -it melodia_database psql -U melodia -d melodia
--- docker/start_docker_postgresql.sh
+++
@@ -1,1 +0,0 @@
-sudo docker create --name melodia_database -e POSTGRES_USER=melodia -e POSTGRES_PASSWORD=melodia -e POSTGRES_DB=melodia -p 5444:5432 -v $(pwd)/docker/data:/var/lib/postgresql/data postgres:16
