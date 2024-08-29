019b81997049641cb92d6021d4b2998a8b176118
DevMaycon <dev.maycon08@gmail.com>
2024-08-28 21:37:53 -0300

    #112938 feat: API temporaria simulando API de musicas

--- flask_app/src/config.py
+++ flask_app/src/config.py
@@ -1,4 +1,5 @@
-from psycopg2 import connect
+import psycopg2
+from requests import request
 
 # Configurações do banco de dados
 DATABASE_CONFIG = {
@@ -10,5 +11,29 @@ DATABASE_CONFIG = {
 }
 
 # Obter conexão com o banco de dados
-def database_connection():
-    return connect(**DATABASE_CONFIG)
+api = psycopg2.connect(**DATABASE_CONFIG)
+
+def pegar_usuario(usuario_id: int):
+    with api:
+        with api.cursor() as sql:
+            sql.execute('SELECT nome FROM usuarios WHERE id=%s', (usuario_id,))
+            resposta = sql.fetchone()
+    return resposta[0]
+            
+
+def preferencias_de_usuarios(usuario_id: int):
+    with api:
+        with api.cursor() as sql:
+            sql.execute('SELECT atividade FROM atividades_usuarios WHERE usuario_id=%s', (usuario_id,))
+            resposta = sql.fetchone()
+    return resposta[0]
+
+def musicas(filtro: str, limit: int = 100):
+    #resposta = request("GET", "")
+    with api:
+        with api.cursor() as sql:
+            generos = tuple([genero for genero in filtro])
+            sql_parametros = ', '.join(['%s'] * len(filtro))
+            sql.execute(f"SELECT id, titulo FROM musicas WHERE genero IN ({sql_parametros}) order by ", generos)
+            resposta = sql.fetchall()
+    return resposta
--- flask_app/src/utils.py
+++ flask_app/src/utils.py
@@ -1,18 +1,29 @@
 from flask import request, jsonify
 
 def validacao_de_parametros(parametros_necessarios: dict):
-    # Verifica todos os parametros necessarios
+    """Verifica todos os parametros necessarios"""
+    
     for parametro in parametros_necessarios.keys():
-        # Verifica http_method` utilizar
+        # Verifica qual http_method utilizar
         if request.method == "GET":
             pegar_parametro = request.args.get(parametro)
         elif request.method == "POST":
-            pegar_parametro = request.form.get(parametro)
+            pegar_parametro = pegar_dado_post(parametro=parametro)
 
         # Emissão de erro caso algum parâmetro necessario esteja vazio
         if not pegar_parametro:
             return jsonify({'status': f'Failed {parametro} parametrer not found'})
         else:
             parametros_necessarios[parametro] = pegar_parametro
-
-
+            
+def pegar_dado_post(parametro: str):
+    """Fornece um dado de uma conexão POST"""
+    
+    # Verifica se o http_method realmente é POST
+    if request.method != "POST":
+        return jsonify({"status": "Seu metodo de conexão não é POST!"})
+    
+    if request.headers.environ["CONTENT_TYPE"] == 'application/json':
+        return request.json.get(parametro)
+    else:
+        return request.form.get(parametro)
